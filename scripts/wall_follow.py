daa3a9450c1b1ce98c8f6050334f1122e848712c
Mauricio Santamaria Lango <mauriciosantamari@gmail.com>
2017-06-27 21:25:15 -0400

    Fixed error by comenting line

--- scripts/wall_follow.py
+++ scripts/wall_follow.py
@@ -111,7 +111,7 @@ class WallFollow():
         self.xs = filtered_ranges * np.sin(filtered_angles)
 
         self.fit_line()
-        self.compute_pd_control()
+        #self.compute_pd_control()
 
         # filter lidar data to clean it up and remove outlisers
         self.received_data = True
