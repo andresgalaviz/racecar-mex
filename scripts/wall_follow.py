cdfed04e4cc449018bef3720fca62d0794f36173
Mauricio Santamaria Lango <mauriciosantamari@gmail.com>
2017-07-05 23:25:15 -0400

    Basic Proportional Controle, using Left and Right walls

--- scripts/wall_follow.py
+++ scripts/wall_follow.py
@@ -6,19 +6,21 @@ from std_msgs.msg import String, Header
 from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
 from sensor_msgs.msg import LaserScan
 
-KP = 0.3
+KP = 0.4
+KD = 0.3
 TARGET_DISTANCE = 3.0
-SPEED = .5
+SPEED = .65
 
 class WallFollow():
     def __init__(self):
         self.pub = rospy.Publisher("/vesc/ackermann_cmd_mux/input/teleop", AckermannDriveStamped, queue_size =1 )
         rospy.Subscriber("/scan",LaserScan, self.reading, queue_size = 1)
 
-    def control(self, RD):
-        pub = rospy.Publisher("/vesc/ackermann_cmd_mux/input/teleop", AckermannDriveStamped, queue_size =1 )
-
-        Error = TARGET_DISTANCE - RD
+    def control(self, RD, LD):
+        #pub = rospy.Publisher("/vesc/ackermann_cmd_mux/input/teleop", AckermannDriveStamped, queue_size =1 )
+        #TARGET_DISTANCE = (RD + LD) / 2
+        #Error = TARGET_DISTANCE - RD
+        Error = LD - RD
         Target_angle = Error * KP
 
         drive_msg_stamped = AckermannDriveStamped()
@@ -30,16 +32,16 @@ class WallFollow():
         
     def reading(self, msg):
         #print msg.ranges
-
         max_a = msg.angle_max
         min_a = msg.angle_min
         a_inc = msg.angle_increment
 
         Right_distance = msg.ranges[int((-np.pi/2 + max_a)/a_inc)]
-        #Right_distance = msg.ranges[int((-np.pi/2 + max_a)/a_inc)]
+        Left_distance = msg.ranges[int((np.pi/2 - max_a)/a_inc)]
+        Front_distance = msg.ranges[int(max_a/a_inc)]
 
-        self.control(Right_distance)
-        #print msg.ranges[Left_distance] , msg.ranges[Right_distance]
+        self.control(Right_distance, Left_distance)
+        #print Left_distance , Right_distance, Front_distance
 
 
 
